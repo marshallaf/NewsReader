4f9723e5a2fb11ff9f7f1de1222c8872d0b17d39
Andrew Marshall <marshall.a.f@gmail.com>
2017-01-16 22:14:22 -0600

    Set text views and add getItem method

--- app/src/main/java/xyz/marshallaf/newsreader/NewsAdapter.java
+++ app/src/main/java/xyz/marshallaf/newsreader/NewsAdapter.java
@@ -2,10 +2,12 @@ package xyz.marshallaf.newsreader;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -38,8 +40,25 @@ public class NewsAdapter extends ArrayAdapter {
             view = inflater.inflate(R.layout.news_list_item, parent, false);
         }
 
+        // get current item
+        Article article = mArticles.get(position);
+
         // set text views
+        ((TextView) view.findViewById(R.id.item_title)).setText(article.getTitle());
+        ((TextView) view.findViewById(R.id.item_subtitle)).setText(article.getSubtitle());
+        ((TextView) view.findViewById(R.id.item_section)).setText(article.getSection());
+        ((TextView) view.findViewById(R.id.item_author)).setText(article.getAuthor());
+        ((TextView) view.findViewById(R.id.item_date)).setText(article.getPublished());
 
         // set image view
+
+
+        return view;
+    }
+
+    @Nullable
+    @Override
+    public Article getItem(int position) {
+        return mArticles.get(position);
     }
 }
